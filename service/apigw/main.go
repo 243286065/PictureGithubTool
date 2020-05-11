7174e4c67eccffe3f69c323caeb3a730afcceefc
xianglai <xianglai@cvte.com>
2020-05-11 11:09:33 +0800

    [update] 支持粘贴板粘贴文件

--- service/apigw/main.go
+++ service/apigw/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	cfg "PictureGithubTool/config"
 	"PictureGithubTool/service/apigw/route"
+	"log"
 )
 
 func main() {
+	log.SetFlags(log.Ldate | log.Lshortfile)
 	router := route.Router()
 	router.Run(cfg.WebServerHost)
 }
